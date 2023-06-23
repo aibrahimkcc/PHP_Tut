456a70c4ef205c0ceacb630948f449de7bddb0b3
Ali İbrahim Keçeci <aibrahimkcc@gmail.com>
2023-06-23 18:44:36 +0300

    1.2

--- Procedural PHP/1.2 Basic PHP Syntax/1.2 Basic PHP Syntax.php
+++ Procedural PHP/1.2 Basic PHP Syntax/1.2 Basic PHP Syntax.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<p>
 <?php
 //echo 'hello world';
 
@@ -12,13 +24,34 @@
 //$_1Ad='Ali';
 //echo $_1Ad;
 
-/*
+/*value assing
 $a = 10;
 $b = $a; // $b, $a'nın değerini alır (bir kopyası)
 $a = 20; // $a'yı değiştirmek, $b'yi etkilemez
 echo $b; // Çıktı: 10 */
 
+/*referans atama
 $a = 1;
 $b = &$a; // "&$"ifadesiyle referans atama gerçekleşir
 $a = 10; // $a'yı değiştirmek, $b'yi etkiler
 echo $b; // Çıktı: 10
+*/
+
+$name = 'kemal';
+echo 'meraba $name';//çıktı:meraba $name
+echo "merhaba $name";//çıktı:merhaba kemal
+echo "merhaba {$name}";//bu da doğru ve daha okunaklı
+//gördüğünüz gibi çift tırnak içinde değişken kullanılabiliyor.
+echo 'merhaba'. $name;
+?>
+
+</p>
+    
+</body>
+</html>
+
+
+
+
+
+
