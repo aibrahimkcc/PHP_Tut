fbddc2896dda30343528f36b3ec47a287bfdb015
Ali İbrahim Keçeci <aibrahimkcc@gmail.com>
2023-06-27 14:14:12 +0300

    1.6

---
+++ Procedural PHP/1.6 Integer Data type/1.6 Integer Data type.php
@@ -0,0 +1,16 @@
+<?php
+echo PHP_INT_MAX;
+echo PHP_INT_MIN;
+echo '<br/>';
+$x = PHP_INT_MAX + 1;
+var_dump($x);
+
+
+
+$decimal=7;
+$octal=0x2a;
+$heksadesimal=0b11;
+
+echo $decimal. "<br/>";
+echo $octal. "<br/>";
+echo $heksadesimal;
