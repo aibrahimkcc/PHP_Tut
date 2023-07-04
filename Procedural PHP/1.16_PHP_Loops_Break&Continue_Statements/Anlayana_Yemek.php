c271d41c169d911192518f7f8e9c83338e647316
Ali İbrahim Keçeci <aibrahimkcc@gmail.com>
2023-07-04 21:22:43 +0300

    update

--- Procedural PHP/1.16_PHP_Loops_Break&Continue_Statements/Anlayana_Yemek.php
+++ Procedural PHP/1.16_PHP_Loops_Break&Continue_Statements/Anlayana_Yemek.php
@@ -5,6 +5,7 @@ $user=[
     'skills'=>['CMK','Aile','PHP', 'kemal']
 ];
 
+$x= count($user['skills'])-1;
 
 foreach($user as $value):
     if(is_array($value)){
@@ -17,3 +18,49 @@ foreach($user as $value):
     echo $value.'<br/>';}
 endforeach;
 
+//chat gpt-version
+
+
+$user = [
+    'name' => 'ali',
+    'email' => 'aibrahimkcc@gmail.com',
+    'skills' => ['CMK', 'Aile', 'PHP', 'kemal']
+];
+
+foreach ($user as $value) {
+    if (is_array($value)) {
+        foreach ($value as $index => $skill) {
+            if ($index === count($value) - 1) {
+                echo $skill;
+            } else {
+                echo $skill . '-';
+            }
+        }
+    } else {
+        echo $value . '<br/>';
+    }
+}
+
+
+//chat gpt-version
+
+$user = [
+    'name' => 'ali',
+    'email' => 'aibrahimkcc@gmail.com',
+    'skills' => ['CMK', 'Aile', 'PHP', 'kemal']
+];
+
+$lastIndex = count($user['skills']) - 1;
+
+foreach ($user as $key => $value) {
+    if (is_array($value)) {
+        foreach ($value as $index => $skill) {
+            echo $skill;
+            if ($index !== $lastIndex) {
+                echo '-';
+            }
+        }
+    } else {
+        echo $value . '<br/>';
+    }
+}
